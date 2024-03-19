f2d39e120781fcc51ca9cda5dbf58c52a996ffbd
YujunSun0 <cons202@gmail.com>
2024-03-19 22:24:10 +0900

    style : 폰트 추가

---
+++ yujunsun-portfolio/src/components/About.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const About = () => {
+  return <Container>about</Container>;
+};
+
+export default About;
+
+const Container = styled.div`
+  height: 100%;
+`;
