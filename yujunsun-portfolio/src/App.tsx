730df31282df6f0d1d1ab0f3c31a30f6c3ce7d08
YujunSun0 <cons202@gmail.com>
2024-03-21 00:25:43 +0900

    feat : 새로고침 시 스크롤 최상단으로 이동

--- yujunsun-portfolio/src/App.tsx
+++ yujunsun-portfolio/src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Portfolio from "./pages/portfolio";
 
 function App() {
+  // 새로고침 시 스크롤을 최상단으로 이동시키는 코드
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Portfolio />} />
