b4330b411c176cb5571938242a90a8ac05726eb3
YujunSun0 <cons202@gmail.com>
2024-03-21 18:44:13 +0900

    docs : assets 폴더 public -> src 이동

--- yujunsun-portfolio/src/index.tsx
+++ yujunsun-portfolio/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./styles/GlobalStyle";
+import "./styles/Font.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
