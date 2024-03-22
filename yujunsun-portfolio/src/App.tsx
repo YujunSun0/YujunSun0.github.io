674c8fc85a4b09286269aa1a47360c90cd2f09b2
YujunSun0 <cons202@gmail.com>
2024-03-22 15:08:09 +0900

    feat : 새로고침시 스크롤 최상단 기능 제거

--- yujunsun-portfolio/src/App.tsx
+++ yujunsun-portfolio/src/App.tsx
@@ -1,15 +1,22 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import ProgressBar from "./components/common/Progress";
 import Portfolio from "./pages/portfolio";
 
 function App() {
-  // 새로고침 시 스크롤을 최상단으로 이동시키는 코드
-  useEffect(() => {
-    window.onbeforeunload = function pushRefresh() {
-      window.scrollTo(0, 0);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const handleBeforeUnload = () => {
+  //     window.scrollTo(0, 0); // 스크롤을 맨 위로 이동
+  //     if (window.location.hash) {
+  //       window.location.hash = ""; // URL 해쉬 초기화
+  //     }
+  //   };
+
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, [navigate]);
 
   return (
     <>
