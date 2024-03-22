be416a486c98c922c4aac542f9b16e4031b5a07b
YujunSun0 <cons202@gmail.com>
2024-03-22 15:11:49 +0900

    style : a태그 기본 속성 제거 및 기타 스타일 변경

--- yujunsun-portfolio/src/styles/GlobalStyle.ts
+++ yujunsun-portfolio/src/styles/GlobalStyle.ts
@@ -16,18 +16,25 @@ const GlobalStyle = createGlobalStyle`
 
 	html {
 		font-size: 62.5%;
+		scroll-behavior: smooth;
 	}
 
 	body {
 		font-size: 1.6rem;
 		overflow: scroll;
 		-ms-overflow-style: none;
+		background-color: rgb(21, 24, 27);
 		font-family: var(--font-NotoSansKR);
 	}
 
 	h1, h2, h3, h4, h5, h6 {
 		font-family: var(--font-JalnanGothic);
 		letter-spacing: 2px;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
 	}
 
 `;
