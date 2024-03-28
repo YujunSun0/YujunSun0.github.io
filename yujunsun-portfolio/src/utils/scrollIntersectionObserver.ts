804e49ada8dd6a6b026fce3fcbe6fe34add329d2
YujunSun0 <cons202@gmail.com>
2024-03-28 19:28:10 +0900

    feat : intersectionObserver 기본동작 방지

--- yujunsun-portfolio/src/utils/scrollIntersectionObserver.ts
+++ yujunsun-portfolio/src/utils/scrollIntersectionObserver.ts
@@ -1,6 +1,4 @@
 export default function scrollIntersectionObserver(callback: () => void) {
-  // const header = document.querySelector("[data-header]");
-
   const options = {
     root: null,
     rootMargin: "80px",
@@ -10,11 +8,14 @@ export default function scrollIntersectionObserver(callback: () => void) {
   const observer = new IntersectionObserver(
     (entries: IntersectionObserverEntry[]) => {
       entries.forEach((entry) => {
+        // 타겟 요소가 루트 요소와 교차하는 점이 없으면 콜백을 호출했으되, 조기에 탈출한다. (기본 동작 방지)
+        if (entry.intersectionRatio <= 0) return;
+
         const hash = "#" + entry.target.id;
         const navEl = document.querySelector(`a[href="/${hash}"]`);
         if (entry.isIntersecting) {
           navEl?.classList.add("focus");
-          callback();
+          callback(); // useNavigate는 일반적인 함수에서 호출할 수 없는 규칙으로 인해 callback으로 받음
         } else {
           navEl?.classList.remove("focus");
         }
